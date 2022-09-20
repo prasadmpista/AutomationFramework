2e6d5c72334459e5fd76860a0b9901958cda5b95
SmartUser <SmartUser@DESKTOP-D5EAA2D>
2022-09-20 09:42:49 +0530

    First commit - Engineer 1

---
+++ src/test/java/SampleTest/ContactTest.java
@@ -0,0 +1,12 @@
+package SampleTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void contact() {
+		
+		System.out.println("this is my first comit - maven projects");
+	}
+
+}
